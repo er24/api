e2c43a04469228fb1507342366c179a16c04bf7e
Er <er.bhingardive@gmail.com>
2018-08-13 16:39:25 +0530

    Product Collection Resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 
 use App\http\Resources\Product\ProductResource;
 
+use App\http\Resources\Product\ProductCollection;
+
 class ProductController extends Controller
 {
     /**
@@ -17,7 +19,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return Product::all();
+        return ProductCollection::collection(Product::all());
     }
 
     /**
