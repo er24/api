fdc9a733503437d890675a2614f8df8ba4af4228
Er <er.bhingardive@gmail.com>
2018-08-13 13:54:20 +0530

    Migrations created for Product and Review tables

--- database/migrations/2018_08_13_075155_create_products_table.php
+++ database/migrations/2018_08_13_075155_create_products_table.php
@@ -14,8 +14,21 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
+
             $table->increments('id');
+
+            $table->string('name');
+
+            $table->text('details');
+
+            $table->integer('price');
+
+            $table->integer('stock');
+
+            $table->integer('discount');
+
             $table->timestamps();
+            
         });
     }
 
