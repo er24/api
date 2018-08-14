07bfe1f0b24d25dc1d7631ac82e2d9f06275859c
Er <er.bhingardive@gmail.com>
2018-08-14 14:02:06 +0530

    Product Destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -127,7 +127,11 @@ class ProductController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function destroy(Product $product)
-    {
-        //
+    {   
+
+        $product->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
+
     }
 }
